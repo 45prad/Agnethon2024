bbab133411a66a6b1e81eaa7af61f6db24d0c193
Richie <richiebt2004@gmail.com>
2024-01-20 22:53:49 +0530

    test

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "";
-$db = "imsportal";
+$password = "root";
+$db = "my_proj_db";
 // Create connection
 $connection = mysqli_connect($servername, $username, $password, $db);
 // Check connection
